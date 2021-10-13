8286690da289bbf38e0ff1c9cfd7163a3af5bdf8
mukul039 <mukulkh900@gmail.com>
2021-10-13 15:08:04 +0530

    intial commit

--- connect.php
+++ connect.php
@@ -6,15 +6,15 @@
 	$number = $_POST['Mobile_Number'];
         $dob = $_POST['Date_OF_Birth'];
         $designation = $_POST['Designation'];
-        $HOBBIES = $_POST['Hobby[]'];
+        $HOBBIES = $_POST['Hobby'];
 	// Database connection
 	$conn = new mysqli('localhost','root','','registration');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, number, dob, designation, HOBBIES) values(?, ?, ?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $number, $dob, $designation, $HOBBIES) values);
+		$stmt = $conn->prepare("insert into user_info(firstName, lastName, gender, email, number, dob, designation, HOBBIES) values(?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("ssssisss", $firstName, $lastName, $gender, $email, $number, $dob, $designation, $HOBBIES);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Registration successfully...";
